7c148de537e92d7c38b84c4599774fac43ca447f
Reuben Jones <mrreubenjones@gmail.com>
2016-10-03 13:34:18 -0700

    Worked on prime function

--- prime.js
+++ prime.js
@@ -1,10 +1,10 @@
 // Write a JavaScript function called: isPrime. The function must take a number n and it should return `true` or `false` whether the argument passed (n) is a prime number or not. A prime number is a number that is divisible only by 1 and itself.
 
 function isPrime(n) {
-  if (n == 1) {
+  if ((n == 1) || (n == 2)) {
     return true;
   } else {
-  for (var i = 2; i < n; i++) {
+  for (var i = 2; i <= n; i++) {
       if (n % i === 0) {
         return false;
       } else {
@@ -48,3 +48,6 @@ console.log("");
 console.log("11 is prime");
 console.log(isPrime(11));
 console.log("");
+
+
+// 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349
