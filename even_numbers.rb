7e6dc2d7a4dec2ab1596cd9343206ee7709f2cd6
Reuben Jones <mrreubenjones@gmail.com>
2016-10-03 13:56:36 -0700

    A final quick fling at the recursive method. Oh, and realizing that question 6 is supposed to be answered in Ruby. Phew.

---
+++ even_numbers.rb
@@ -0,0 +1,56 @@
+# // Implement question #5 again in Ruby in two ways:
+# //     a. Using a loop
+# //     b. Using recursion
+# //    Benchmark the two solutions (include your code for the benchmarking). Which one of your solutions is faster?
+
+# // Loop
+  def even_numbers(n)
+    for i in 1..n
+      puts i*2
+    end
+  end
+
+# Recursion
+
+  def even_numbers_recursive(n)
+    if n == 0
+      0
+    else
+      n % 2 == 0 ? n : even_numbers_recursive(n-1)
+    end
+  end
+
+  even_numbers(1)
+  puts
+  even_numbers(2)
+  puts
+  even_numbers(3)
+  puts
+  even_numbers(4)
+  puts
+  even_numbers(5)
+
+  # even_numbers_recursive(1)
+  # puts
+  # even_numbers_recursive(2)
+  # puts
+  # even_numbers_recursive(3)
+  # puts
+  # even_numbers_recursive(4)
+  puts
+  even_numbers_recursive(5)
+
+
+
+# Benchmarking: This is how I would do it, if I could get the damned recursion algorithmn right.
+require 'benchmark'
+
+n = 1_000_000
+
+Benchmark.bm do |x|
+  x.report { n.times { even_numbers(10) }}
+  x.report { n.times { even_numbers_recursive(10) }}
+end
+
+
+
