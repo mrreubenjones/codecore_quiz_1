196613608fe9014a85c6b28270f99e22524eeca2
Reuben Jones <mrreubenjones@gmail.com>
2016-10-03 10:32:40 -0700

    First attempt at even numbers question

---
+++ even_numbers.js
@@ -0,0 +1,17 @@
+// Write a function in JavaScript that takes in a number n and returns the first n even numbers.
+
+function evenNumbers(n) {
+  for (var i = 1; i <= n; i++) {
+      console.log(i*2);
+  }
+}
+
+evenNumbers(1);
+console.log();
+evenNumbers(2);
+console.log();
+evenNumbers(3);
+console.log();
+evenNumbers(4);
+console.log();
+evenNumbers(5);
