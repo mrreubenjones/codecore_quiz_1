2545d70be7f445ceb336500976d8d2993c569dff
Reuben Jones <mrreubenjones@gmail.com>
2016-10-03 18:39:07 -0700

    Finally! Figured out recursion, and tested benchmarking

--- even_numbers.rb
+++ even_numbers.rb
@@ -3,54 +3,74 @@
 # //     b. Using recursion
 # //    Benchmark the two solutions (include your code for the benchmarking). Which one of your solutions is faster?
 
-# // Loop
-  def even_numbers(n)
-    for i in 1..n
-      puts i*2
-    end
+#  Loop method
+puts "Loop"
+def even_numbers(n)
+  for i in 1..n
+    puts i*2
   end
-
-# Recursion
-
-  def even_numbers_recursive(n)
-    puts 2
-    if n != 0
-      2 * even_numbers_recursive(n)
-    end
-    n -= 1
-  end
-
-  even_numbers(1)
-  puts
-  even_numbers(2)
-  puts
-  even_numbers(3)
-  puts
-  even_numbers(4)
-  puts
-  even_numbers(5)
-
-  # even_numbers_recursive(1)
-  # puts
-  # even_numbers_recursive(2)
-  # puts
-  # even_numbers_recursive(3)
-  # puts
-  # even_numbers_recursive(4)
-  puts
-  even_numbers_recursive(5)
-
-
-
-# Benchmarking: This is how I would do it, if I could get the damned recursion algorithmn right.
-require 'benchmark'
-
-n = 1_000_000
-
-Benchmark.bm do |x|
-  x.report { n.times { even_numbers(10) }}
-  x.report { n.times { even_numbers_recursive(10) }}
 end
 
+even_numbers(1)
+puts
+even_numbers(2)
+puts
+even_numbers(3)
+puts
+even_numbers(4)
+puts
+even_numbers(5)
+puts
+
+#  Recursive method
+puts "Recursion"
+def even_numbers_recursive(n)
+  even_numbers_recursive(n-1) unless n == 1
+  puts n * 2
+end
+
+puts even_numbers_recursive(1)
+puts even_numbers_recursive(2)
+puts even_numbers_recursive(3)
+puts even_numbers_recursive(4)
+puts even_numbers_recursive(5)
+
+
+
+
+
+
+
+
+# Benchmarking Result: Looping is much faster
+# user     system      total        real
+# Loop * 10_000_000:
+  #  2.900000   0.010000   2.910000 (  2.908734)
+# Recursion * 10_000_000
+  #  5.510000   0.000000   5.510000 (  5.520155)
+
+
+# # Loop method----------------------
+# def even_numbers(n)
+#   for i in 1..n
+#     return i*2
+#   end
+# end
+#
+# # Recursion----------------------
+# def even_numbers_recursive(n)
+#   even_numbers_recursive(n-1) unless n == 1
+#   n * 2
+# end
+#
+# require "benchmark"
+#
+# y = 10_000_000
+#
+# Benchmark.bm do |x|
+#   x.report { y.times { even_numbers(10) }}
+#   x.report { y.times { even_numbers_recursive(10) }}
+# end
+#
 
 
